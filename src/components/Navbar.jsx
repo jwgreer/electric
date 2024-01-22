4a658937d53e6970ff60ac2826dc513eec5da16a
Jordan Greer <jwgreer2@gmail.com>
2024-01-22 10:18:35 -0700

    created project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="/">Electric Road Trip</a>
+        </div>
+        </nav>
+    );
+};
+
+export default Navbar;
